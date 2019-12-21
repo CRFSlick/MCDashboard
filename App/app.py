dc2904c2b17e03b108410eba8d8e2723f1755fd6
CRFSlick <47066085+CRFSlick@users.noreply.github.com>
2019-12-21 02:31:57 -0600

    Added alerts and more test buttons! :D

--- App/app.py
+++ App/app.py
@@ -1,11 +1,33 @@
-from flask import Flask
 from flask import render_template
+from flask import jsonify
+from flask import request
+from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
+
+
+@app.route('/api/console')
+def api_cmd():
+    cmd = request.args.get('cmd')
+    print(f'CMD: {cmd}')
+
+    if cmd != '':
+        response = {'cod': '200'}
+    else:
+        response = {
+            'cod': '400',
+            'error': {
+                'type': 'Data Error',
+                'msg': 'The server has received bad data'
+            }
+        }
+
+    print(response)
+    return jsonify(response)
 
 
 if __name__ == "__main__":
--- App/static/js/main.js
+++ App/static/js/main.js
@@ -12,6 +12,10 @@ bootstrapColors = {
 
 $(document).ready(function () {
 
+    alert_event = setTimeout(function() {
+		$('.alert').fadeOut();
+	  }, 0);
+
     reportCircles = {
         'cpu': createReportCircle('cpu'),
         'ram': createReportCircle('ram'),
@@ -20,20 +24,120 @@ $(document).ready(function () {
     };
 
     $('#testbutton').click(function () {
-        setCircle('cpu', 24)
+        setCircle('cpu', 2.8);
     });
 
+    $('#testbutton0').click(function () {
+        setCircle('cpu', 52);
+    });
     $('#testbutton1').click(function () {
-        setCircle('disk', 80)
+        setCircle('ram', 79);
     });
 
     $('#testbutton2').click(function () {
-        setCircle('ram', 98)
+        setCircle('disk', 23);
     });
 
     $('#testbutton3').click(function () {
-        setCircle('players', 8)
+        setCircle('players', 8);
     });
+
+    $('#testbutton4').click(function () {
+        setCircle('cpu', 52);
+        setCircle('ram', 79);
+        setCircle('disk', 23);
+        setCircle('players', 8);
+    });
+
+    $('#testbutton5').click(function () {
+        setCircle('cpu', 0);
+        setCircle('ram', 0);
+        setCircle('disk', 0);
+        setCircle('players', 0);
+    });
+
+    $('#testbutton6').click(function () {
+        $.ajax({
+            data : {
+                cmd : $('#command-input').val()
+            },
+            type : 'GET',
+            url : '/api/console',
+            timeout: 15000
+        })
+        .done(function(data) {
+            // stopSpinner('search');
+    
+            if (data.cod == '200') {
+                alertSuccess('Success', 'Data submitted!');
+                console.log(data);
+            }
+            else {
+                alertError(data.error.type, data.error.msg);
+                // hideAlerts();
+                // hideElementsForSearch();
+                // $('*').promise().done(function(){
+                //     displayData(data.api_response);
+                //     LAST_API_RESPONSE = data.api_response;
+                // });
+                // showWeatherCard();
+            }
+        }).fail(function(jqXHR, textStatus){
+            // stopSpinner('search');
+            if(textStatus === 'timeout')
+            {
+                alertError('Timeout Error', 'The server took too long to respond. Please try again!');
+            }
+            else {
+                alertError('Internal Server Error', 'Something went wrong on our end. Please try again later.');
+            }
+        });
+    })
+
+    function clearAlerts() {
+        clearTimeout(alert_event);
+        $('.alert').each(function () {
+            $(this).fadeOut()
+        })
+    }
+
+    function alertSuccess(type, msg) {
+        clearAlerts();
+        $('*').promise().done(function() {
+            type = `${type}: `;
+            $('#successAlert > #alert').text(type);
+            $('#successAlert > #msg').text(msg);
+            $('#successAlert').fadeIn();
+            alert_event = setTimeout(function() {
+                $('#successAlert').fadeOut();
+              }, 8000);
+        });
+    }
+
+    function alertError(type, msg) {
+        clearAlerts();
+        $('*').promise().done(function() {
+            type = `${type}: `;
+            $('#errorAlert > #alert').text(type);
+            $('#errorAlert > #msg').text(msg);
+            $('#errorAlert').fadeIn();
+            alert_event = setTimeout(function() {
+                $('#errorAlert').fadeOut();
+              }, 8000);
+        });
+    }
+
+    function alertWarning(warning) {
+        clearAlerts();
+        $('*').promise().done(function() {
+            clearTimeout(warning_event);
+            $('#warningAlert').text(warning).fadeIn();
+            warning_event = setTimeout(function() {
+                $('#warningAlert').fadeOut();
+              }, 8000);
+        });
+    }
+
 });
 
 function createReportCircle(id) {
@@ -41,15 +145,28 @@ function createReportCircle(id) {
     var total;
     var color;
     var symbol;
+    var fontSize;
     var circleValues = {}
 
     if (id == 'players') {
         total = 20;
         symbol = `/${total}`;
+        fontSize = 2
+    } 
+    else if (id == 'ram') {
+        total = 16;
+        symbol = `/${total}gb`;
+        fontSize = 2
+    }
+    else if (id == 'disk') {
+        total = 500;
+        symbol = `/${total}gb`;
+        fontSize = 1.5
     }
     else {
         total = 100;
         symbol = '%';
+        fontSize = 2
     }
 
     if ($(`#container.circle-report.${id}`).hasClass('primary')) {
@@ -112,7 +229,8 @@ function createReportCircle(id) {
 
             bar.text.style.color = state.color;
             bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
-            bar.text.style.fontSize = '2rem';
+            bar.text.style.fontSize = `${fontSize}rem`;
+            bar.text.style.top = '';
         }
     }
 
